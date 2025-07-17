1e1cd04a400a3ecd4573e2b54b69be7a5c33fd0f
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-07-17 11:28:24 +0200

    update

--- Numeri_Random_Bottone.ino
+++ Numeri_Random_Bottone.ino
@@ -16,13 +16,12 @@ void loop()
 
  if(digitalRead(PinButton) == HIGH)
  {
-   int numero = random(1, 7); //numero casuale tra 1 e 6
    lcd_1.clear();
-   lcd_1.setCursor(4, 0);
-   lcd_1.print("Numero ");
-   lcd_1.setCursor(6, 1);
+   int numero = random(1, 7); //numero casuale tra 1 e 6
+   lcd_1.setCursor(2, 0);
+   lcd_1.print("Numero: ");
    lcd_1.print(numero);
-   delay(3000); //attendi un secondo prima di cancellare il display
+   delay(2000);
    lcd_1.clear();
  }
  else if(digitalRead(PinButton) == LOW)
