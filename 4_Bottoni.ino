b5f139a2fb896366f0b9ec5589daff52deb40d1f
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-07-18 12:41:59 +0200

    update

--- 4_Bottoni.ino
+++ 4_Bottoni.ino
@@ -3,33 +3,40 @@
 // Inizializza l'oggetto LCD con gli indirizzi dei pin (in questo caso 0, ma di solito sono altri)
 Adafruit_LiquidCrystal lcd(0);
 
-// inserire variabili 
-// .
-// .
-// .
-// .
-
-// funzione reset per tornare allo stato iniziale
-void resetStato() {
-
-  Contatore = 0; // Azzera il contatore
-  lcd.clear();   // Pulisce il display
-  lcd.print("cambiare testo"); // Scrive il messaggio iniziale
-  
-}
+int su = 2;
+int giu = 3;
+int annulla = 4;
+int conferma = 5;
 
 void setup() {
-  // Inizializza il display LCD con 16 colonne e 2 righe
-  lcd.begin(16, 2);
-  
-  // Imposta i pin dei bottoni come INPUT
-  pinMode(------------, ------------);
-  resetStato();
-  delay(1000);
+
+ 
+ pinMode(2, INPUT); //bottone su
+ pinMode(3, INPUT); //bottone giu
+ pinMode(4, INPUT); //bottone annulla
+ pinMode(5, INPUT); //bottone conferma
+ lcd.begin(16, 2);
+ lcd.clear();
+ lcd.print("cambiare testo"); // Scrive il messaggio iniziale
+
 }
 
 
 void loop() {
+
+if (digitalRead(conferma) == HIGH)
+{
+  lcd.clear();
+  lcd.print("conferma");
+}
+
+else if (digitalRead(annulla) == HIGH;)
+{
+   lcd.clear();
+   lcd.print("annulla");
+}
+
+
 
 
 }
