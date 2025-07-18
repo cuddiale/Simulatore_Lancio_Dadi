97f7014e8ef1ac45cfee531db4a263603cb0aa22
alessio cuddretto <alessiocuddretto@hotmail.com>
2025-07-18 11:40:21 +0200

    update

--- 4_Bottoni.ino
+++ 4_Bottoni.ino
@@ -1,7 +1,89 @@
-void setup() {
+#include <Adafruit_LiquidCrystal.h>
+
+// Inizializza l'oggetto LCD con gli indirizzi dei pin (in questo caso 0, ma di solito sono altri)
+Adafruit_LiquidCrystal lcd(0);
+
+// inserire variabili 
+// .
+// .
+// .
+// .
+
+// funzione reset per tornare allo stato iniziale
+void resetStato() {
+
+  Contatore = 0; // Azzera il contatore
+  lcd.clear();   // Pulisce il display
+  lcd.print("cambiare testo"); // Scrive il messaggio iniziale
   
 }
 
-void loop() {
+void setup() {
+  // Inizializza il display LCD con 16 colonne e 2 righe
+  lcd.begin(16, 2);
   
+  // Imposta i pin dei bottoni come INPUT
+  pinMode(------------, ------------);
+  resetStato();
+  delay(1000);
 }
+
+
+void loop() {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
